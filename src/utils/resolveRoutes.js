4eb933024477f131ecba99232fbe2002cfb26eac
David Valencia <codingdavid9@outlook.com>
2021-05-19 10:02:20 -0500

    Notes/Conectar las rutas con los templates

--- src/utils/resolveRoutes.js
+++ src/utils/resolveRoutes.js
@@ -1,6 +1,6 @@
 const resolveRoutes = (route) => {
-  if (route.length <= 3) {
-    let validRoute = route === '/' ? route : '/:id';
+  if (route.length <= 3) { //Si la cantidad de elementos de route es menor o igual que 3, entonces...
+    let validRoute = route === '/' ? route : '/:id'; //Si es igual en valor y tipo a "/" una pagina me retorna, en caso contrario si es  un "/:id" un personaje me retorna este
     return validRoute;
   }
   return `/${route}`;
